eff972fee7a700a6d8d850f10c60f507573ddf2b
Edward Fan <iss988fan@gmail.com>
2014-08-21 16:30:56 -0400

    Switched to bottle
    This should work..?

--- streambrowser.py
+++ streambrowser.py
@@ -1,16 +1,13 @@
-import urllib.request
-import json
-
-streams = {}
-
-def update():
+def streambrowser():
+    import urllib2
+    import json
+    
     url = "https://api.twitch.tv/kraken/streams?game=Pok%C3%A9mon%20X%2FY"
     
-    f = urllib.request.urlopen(url)
-    charset = f.info().get_param('charset', 'utf8')
+    f = urllib2.urlopen(url)
     f_obj = f.read()
     
-    data = json.loads(f_obj.decode(charset)).get('streams')
+    data = json.loads(f_obj.decode('utf-8')).get('streams')
 
     streamdata = []
 
@@ -19,5 +16,14 @@ def update():
 
     for stream in streamdata:
         print(stream)
+
+    table = '<table><tr><th>Streamer</th><th>Viewers</th></tr>'
+    for stream in streamdata:
+        table = table + '<tr><td><a href="' + stream[2] + '">' + stream[0] + '</a></td><td>' + str(stream[1]) + '</td></tr>'
+    table = table + '</table>'
+
+    return(table)
     
-update()
+
+if __name__ == '__main__':
+    streambrowser()
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/iss/Pokemon Stream Browser/venv"
+set -gx VIRTUAL_ENV "/Users/iss/Pokemon-Stream-Browser/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
